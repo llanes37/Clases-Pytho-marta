8db58b9553fac94e4feaa95a9c1c538c41591168
llanes37 <78422866+llanes37@users.noreply.github.com>
2025-07-17 15:59:08 +0200

    subida

---
+++ variables.py
@@ -0,0 +1,19 @@
+nombre = "marta"
+introducirNombre = input("Ingresa una cadena de texto: ")
+hermano = "javier"
+
+# Suma
+print(2+2) # 4
+# Resta
+print(10-2-5) # 3
+# Multiplicación
+print(8*3) # 24
+# División
+print(30/6) # 5.0 la división siempre devuelve un float
+# Módulo, resto o residuo
+print(21%5) # 1 
+# División Entera
+print(21//5) # 4 devuelve un int
+# Potencia
+print(2**3) # 8
+
